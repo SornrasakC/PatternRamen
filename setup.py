303cd1cfc32b565c64b47b83756c4453afa35946
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2022-04-06 23:26:36 +0700

    setup py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='PatternRamen',
+   version='1.0',
+   description='A useful module',
+   author='Man Foo',
+   author_email='foomail@foo.com',
+)
+
+#    packages=['PatternRamen'],  #same as name
+#    install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+#    scripts=[
+#             'scripts/cool',
+#             'scripts/skype',
+#            ]
