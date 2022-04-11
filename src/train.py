ceabbff08d88679a3eadca702dd62b2b6c7d9b98
Sav-eng <save-000@hotmail.com>
2022-04-11 18:54:39 +0700

    fix bug training script

--- src/train.py
+++ src/train.py
@@ -14,6 +14,7 @@ class Training():
     self.discriminator_color = Discriminator()
     self.generator = Generator()
     self.vgg16 = models.vgg16(pretrained=True).features[:25]
+    self.vgg16.cuda()
     self.vgg16.eval()
     self.discriminator_line.cuda()
     self.discriminator_color.cuda()
@@ -33,21 +34,21 @@ class Training():
     for _it in range(iterations):
       line, color, transform_color, noise = next(dataLoader)
       d_optimizer_line.zero_grad()
-      generated_image = self.generator(line, transform_color, noise)
-      d_loss_line = (self.discriminator_line(line, color)-1)**2 + self.discriminator_line(line, generated_image)**2
-      d_loss_line.backward()
-      d_optimizer_line.step()
-      
       d_optimizer_color.zero_grad()
-      d_loss_color = (self.discriminator_color(color, color)-1)**2 + self.discriminator_color(color, generated_image)**2
-      d_loss_color.backward()
-      d_optimizer_color.step()
+
+      generated_image = self.generator(line, transform_color, noise)
+
+      d_loss_line = torch.mean((self.discriminator_line(line, color)-1)**2 + self.discriminator_line(line, generated_image.detach())**2)
+      d_loss_color = torch.mean((self.discriminator_color(color, color)-1)**2 + self.discriminator_color(color, generated_image.detach())**2)
       
       d_loss = d_loss_line + d_loss_color
+      d_loss.backward()
+      d_optimizer_color.step()      
+      d_optimizer_line.step()
       
       g_optimizer.zero_grad()
-      p_loss = self.perceptual_criterion(self.vgg16(color), self.vgg16(generated_image), p=1, dim=2)
-      g_loss = (self.discriminator(line, generated_image) - 1)**2 + p_loss
+      p_loss = torch.mean(self.perceptual_criterion(self.vgg16(color), self.vgg16(generated_image)))
+      g_loss = torch.mean((self.discriminator_line(line, generated_image) - 1)**2) + torch.mean((self.discriminator_color(color, generated_image) - 1)**2) + p_loss
       g_loss.backward()
       g_optimizer.step()
       
@@ -64,6 +65,7 @@ class Training():
   def inference(self, dataLoader):
     self.generator.eval()
     pics = []
-    for line, ref, noise in next(dataLoader):
-      pics.append(self.generator(line, ref, noise).detach().cpu())
+    for _it in range(10):
+      line, color, noise = next(dataLoader)
+      pics.append(self.generator(line, color, noise).squeeze().permute(1,2,0).detach().cpu())
     return pics
