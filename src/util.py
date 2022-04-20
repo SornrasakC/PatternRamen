1fd50ebe36ea732a74b291fe55a17d78838d96c7
Megacodexz <megacodexz@gmail.com>
2022-04-20 14:45:49 +0700

    set fig size

--- src/util.py
+++ src/util.py
@@ -8,6 +8,8 @@ def show_image(img):
 
 def show_image_row(imgs):
   f, axs = plt.subplots(1, len(imgs))
+  f.set_figheight(6)
+  f.set_figwidth(15)
   for ax, img in zip(axs, imgs):
       ax.imshow(img)
   plt.show() # or display.display(plt.gcf()) if you prefer
