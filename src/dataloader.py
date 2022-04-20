Add normalization and fix a bug

src/dataloader.py
@@ -60,15 +60,16 @@ class XDoGData:
         self.train_transform = torch.jit.script(train_transform)
 
         self.transform = transforms.Compose([
-            transforms.ToPILImage(),
             transforms.Resize((256,256)),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                std=[0.5, 0.5, 0.5])
         ])
         # rotate = nn.Sequential(transforms.RandomRotation(60,fill=255))
         # self.rotate = torch.jit.script(rotate)
 
     def __getitem__(self, idx):
-        img = cv2.imread(self.folder_path + "/" + self.data[idx], cv2.COLOR_BGR2RGB)
+        img = Image.open(self.folder_path + "/" + self.data[idx]).convert('RGB')
         sigma_rand = np.random.uniform(self.sigma, self.sigma + 0.2)
         noise = np.random.normal(0, 1, 256)
         is_xdog = random.choice([True, False])