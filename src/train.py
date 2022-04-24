d6c73a15e27567b6499d41971a09390307839317
SornrasakC <sronrasak@gmail.com>
2022-04-25 00:10:52 +0700

    add checkpoints

--- src/train.py
+++ src/train.py
@@ -1,15 +1,16 @@
 from src.model import Discriminator, Generator
 from torch import optim
-from src.util import show_image, show_image_row
+from src.util import show_image, show_image_row, pack_checkpoint, gen_checkpoint_path
 from torch.nn import functional as F
 from torch import nn
 import torch
 import wandb
 import torchvision.models as models
 from tqdm import tqdm
+import os
 
 class Training():
-  def __init__(self):
+  def __init__(self, checkpoint_path=None):
     self.discriminator_line = Discriminator()
     self.discriminator_color = Discriminator()
     self.generator = Generator()
@@ -21,15 +22,23 @@ class Training():
     self.generator.cuda()
     self.perceptual_criterion = nn.L1Loss()
 
+    self.init_optimizers()
+
+    self.iteration = 0
+
+    if checkpoint_path is not None:
+      self.load_checkpoint(checkpoint_path)
+
+  def init_optimizers(self):
+    self.g_optimizer = optim.Adam(self.generator.parameters(), lr=1e-3, betas=(0.5, 0.999)) # paper lr 1e-4
+    self.d_optimizer_line = optim.Adam(self.discriminator_line.parameters(), lr=1e-4, betas=(0.5, 0.999)) # paper lr 4e-4
+    self.d_optimizer_color = optim.Adam(self.discriminator_color.parameters(), lr=1e-4, betas=(0.5, 0.999)) # paper lr 4e-4
+
   def train(self, dataLoader, valDataLoader, iterations):
     wandb.init(project="colorization")
     wandb.watch(self.discriminator_line)
     wandb.watch(self.discriminator_color)
     wandb.watch(self.generator)
-    
-    g_optimizer = optim.Adam(self.generator.parameters(), lr=1e-3, betas=(0.5, 0.999)) # paper lr 1e-4
-    d_optimizer_line = optim.Adam(self.discriminator_line.parameters(), lr=1e-4, betas=(0.5, 0.999)) # paper lr 4e-4
-    d_optimizer_color = optim.Adam(self.discriminator_color.parameters(), lr=1e-4, betas=(0.5, 0.999)) # paper lr 4e-4
     
     for _it in tqdm(range(iterations)):
       self.generator.train()
@@ -39,8 +48,8 @@ class Training():
       transform_color = transform_color.cuda().to(dtype=torch.float32)
       noise = noise.cuda().to(dtype=torch.float32)
       
-      d_optimizer_line.zero_grad()
-      d_optimizer_color.zero_grad()
+      self.d_optimizer_line.zero_grad()
+      self.d_optimizer_color.zero_grad()
 
       generated_image = self.generator(line, transform_color, noise)
 
@@ -49,31 +58,39 @@ class Training():
       
       d_loss = (d_loss_line + d_loss_color) / 2
       d_loss.backward()
-      d_optimizer_color.step()      
-      d_optimizer_line.step()
+      self.d_optimizer_color.step()      
+      self.d_optimizer_line.step()
       
-      g_optimizer.zero_grad()
+      self.g_optimizer.zero_grad()
       p_loss = torch.mean(self.perceptual_criterion(self.vgg16(color), self.vgg16(generated_image)))
       pure_g_loss = torch.mean((self.discriminator_line(line, generated_image) - 1)**2) + torch.mean((self.discriminator_color(color, generated_image) - 1)**2)
       g_loss = pure_g_loss + 1 * p_loss #/ (16 * 16) # [BS, 512, 16, 16]
       g_loss.backward()
-      g_optimizer.step()
+      self.g_optimizer.step()
       
       wandb.log({"g_loss": g_loss, "d_loss": d_loss})
       if(_it % 100==0):
-        print("Iteration: {}/{}".format(_it, iterations), "g_loss: {:.4f}".format(g_loss), "d_loss: {:.4f}".format(d_loss))
+        print("Iteration: {}/{}".format(_it + self.iteration, iterations + self.iteration), "g_loss: {:.4f}".format(g_loss), "d_loss: {:.4f}".format(d_loss))
       
       if(_it % 100==0):
         pic_rows = self.inference(valDataLoader)
         for pic_row in pic_rows[:10]:
           show_image_row(pic_row)
+
+      if(_it % 100==0):
+        self.save_checkpoint(iteration=self.iteration + _it)
+
     wandb.finish()
+    self.iteration += iterations
+    self.save_checkpoint()
   
   def inference(self, dataLoader):
     self.generator.eval()
-    pics = []
+    pic_row = []
     with torch.no_grad():
       for _it in range(10):
+        if len(pic_row) > 10:
+          break
         line, color, noise = next(dataLoader)
         line = line.cuda().to(dtype=torch.float32)
         color = color.cuda().to(dtype=torch.float32)
@@ -87,5 +104,34 @@ class Training():
           gen_im = un_norm_im(gen_im)
           line_im = un_norm_im(line_im)#.type(torch.int)
           color_im = un_norm_im(color_im)#.type(torch.int)
-          pics.append([line_im, color_im, gen_im])
-    return pics
+          pic_row.append([line_im, color_im, gen_im])
+    return pic_row
+
+  def save_checkpoint(self, iteration=..., filepath=...):
+    if iteration is ...:
+      iteration = self.iteration
+
+    if filepath is ...:
+      filepath = gen_checkpoint_path(iteration)
+    
+    pack_dict = pack_checkpoint(self.discriminator_line, self.discriminator_color, self.generator, self.g_optimizer, self.d_optimizer_line, self.d_optimizer_color, iteration)
+
+    torch.save(pack_dict, filepath)
+
+  def load_checkpoint(self, checkpoint_path):
+    assert os.path.isfile(checkpoint_path)
+
+    pack_dict = torch.load(checkpoint_path, location='cpu')
+
+    self.discriminator_line.load_state_dict(pack_dict['discriminator_line'])
+    self.discriminator_color.load_state_dict(pack_dict['discriminator_color'])
+    self.generator.load_state_dict(pack_dict['generator'])
+
+    self.init_optimizers()
+
+    self.g_optimizer.load_state_dict(pack_dict['g_optimizer'])
+    self.d_optimizer_line.load_state_dict(pack_dict['d_optimizer_line'])
+    self.d_optimizer_color.load_state_dict(pack_dict['d_optimizer_color'])
+
+    self.iteration = pack_dict['iteration']
+
--- src/util.py
+++ src/util.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import os
 
 def show_image(img):
   imgplot = plt.imshow(img)
@@ -14,3 +14,22 @@ def show_image_row(imgs):
       ax.imshow(img)
       ax.axis('off')
   plt.show() # or display.display(plt.gcf()) if you prefer
+
+
+def pack_checkpoint(discriminator_line, discriminator_color, generator, g_optimizer, d_optimizer_line, d_optimizer_color, iteration):
+  pack_dict = {
+    'discriminator_line': discriminator_line,
+    'discriminator_color': discriminator_color,
+    'generator': generator,
+    'g_optimizer': g_optimizer.state_dict(),
+    'd_optimizer_line': d_optimizer_line.state_dict(),
+    'd_optimizer_color': d_optimizer_color.state_dict(),
+    'iteration': iteration,
+  }
+
+  return pack_dict
+
+def gen_checkpoint_path(iteration):
+  target = f'checkpoints/checkpoint-{iteration}'
+  checkpoint_fp = os.path.join(os.getcwd(), target)
+  return checkpoint_fp
